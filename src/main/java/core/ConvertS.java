b9dea8c570647dd1c0f56b9929984cf0154b3c86
PFName PLNmae <PFNamePLNmae@gmail.com>
2015-10-18 10:22:44 -0700

    HW_45_JSON_Parsing

---
+++ src/main/java/core/ConvertS.java
@@ -0,0 +1,164 @@
+//BEGIN
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class ConvertS {
+
+	// read URL
+	public final String[] urlS = ReadURL();
+
+	private static String[] ReadURL() {
+		ArrayList<String> arrS2D = new ArrayList<String>();
+
+		String csvFile = "./src/main/resources/URL.csv";
+		BufferedReader br = null;
+		String line = null;
+		String[] csv = null;
+		String SplitBy = "!#";
+
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			while ((line = br.readLine()) != null) {
+				csv = line.split(SplitBy);
+				if (csv.length == 1) {
+					arrS2D.add(csv[0]);
+				} else {
+					for (int i = 0; i < csv.length; i++)
+						arrS2D.add(csv[i]);
+				}
+			} // while ((line = br.readLine()) != null) {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // try {
+
+		// convert
+		String[] URLS2D = new String[arrS2D.size()];
+		arrS2D.toArray(URLS2D);
+		return URLS2D;
+	}
+
+	// showElement
+	public static void showElement(WebDriver driver, WebElement element) {
+		final String originalBackgroundColor = element
+				.getCssValue("backgroundColor");
+		final JavascriptExecutor romaTestJS = ((JavascriptExecutor) driver); // java
+																				// script
+		romaTestJS.executeScript(
+				"arguments[0].style.backgroundColor = 'rgb(0,200,0)'", element);
+
+		try {
+			Thread.sleep(45);
+		} catch (InterruptedException e) {
+		}
+		romaTestJS.executeScript("arguments[0].style.backgroundColor = '"
+				+ originalBackgroundColor + "'", element);
+	}
+
+	public String[][] ConvertSToS2D() {
+
+		// ArrayList
+		ArrayList<String[]> arrS2D = new ArrayList<String[]>();
+		WebElement element = null;
+		String itemName = null;
+		double itemPriceUSD = 0;
+
+		// JSON
+		String[][] JSONS2D = JSON.JSONS2D();
+		// JSON
+
+		// WebDriver
+		WebDriver dr = null;
+		String tWD = System.getProperties().getProperty("typeWebDriver");
+		if ((tWD == null)) {
+			// class org.openqa.selenium.htmlunit.HtmlUnitDriver
+			dr = new HtmlUnitDriver();
+			// ((HtmlUnitDriver) dr).setJavascriptEnabled(true);
+		} else if ((tWD.equals("Firefox")) // ---
+				|| (tWD.equals("Fox"))
+				|| (tWD.equals("fox"))
+				|| (tWD.equals("FirefoxDriver"))) {
+			// class org.openqa.selenium.firefox.FirefoxDriver
+			dr = new FirefoxDriver();
+		} else {
+			// class org.openqa.selenium.htmlunit.HtmlUnitDriver
+			dr = new HtmlUnitDriver();
+			// ((HtmlUnitDriver) dr).setJavascriptEnabled(true);
+		}
+		// WebDriver
+
+		double doubleOld;
+		double doubleNew;
+		for (int i = 0; i < urlS.length; i++) {
+			// URL
+			// dr.get("http://www.amazon.com/Withings-70031601-Pulse-Heart-Monitor/dp/B00KBN1F5A/ref=pd_sim_sbs_sg_6?ie=UTF8&refRID=0VQXF03E0KCG4S8JZKF4");
+			dr.get(urlS[i]);
+
+			// поиск элемента
+			element = dr.findElement(By.id("productTitle"));
+			itemName = element.getText();
+			if (dr.getClass().toString()
+					.equals("class org.openqa.selenium.firefox.FirefoxDriver"))
+				showElement(dr, element);
+			element = dr.findElement(By.id("priceblock_ourprice"));
+			if (dr.getClass().toString()
+					.equals("class org.openqa.selenium.firefox.FirefoxDriver"))
+				showElement(dr, element);
+			itemPriceUSD = Double.parseDouble(element.getText().substring(1,
+					element.getText().length()));
+
+			// запись
+			arrS2D.add(new String[] { "", "Name: " + itemName,
+					"Name: " + itemName });
+			arrS2D.add(new String[] { "", "US Prise: " + itemPriceUSD,
+					"US Prise: " + itemPriceUSD });
+			arrS2D.add(new String[] { "", "Country: " + JSONS2D[i][2],
+					"Country: " + JSONS2D[i][2] });
+			doubleOld = itemPriceUSD * Double.parseDouble(JSONS2D[i][7]);
+			doubleNew = new BigDecimal(doubleOld).setScale(2,
+					RoundingMode.HALF_EVEN).doubleValue();
+			arrS2D.add(new String[] { "", "Local Price: " + doubleNew,
+					"Local Price: " + doubleNew });
+		} // for (int i = 0; i < urlS.length; i++) {
+
+		// close
+		dr.close();
+
+		// convert
+		String[][] S2D = new String[arrS2D.size()][];
+		arrS2D.toArray(S2D);
+		return S2D;
+	}
+
+	public static void main(String[] args) {
+		ConvertS conv = new ConvertS();
+		String[][] str = conv.ConvertSToS2D();
+		int n = conv.urlS.length;
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Item_" + i + ": " + str[i * 4 + 0][2] + "; "
+					+ str[i * 4 + 1][2] + "; " + str[i * 4 + 2][2] + "; "
+					+ str[i * 4 + 3][2] + ";");
+		}
+	}
+}
+// END
---
+++ src/main/java/core/JSON.java
@@ -0,0 +1,162 @@
+//BEGIN
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
+
+public class JSON {
+
+	private static String[] ReadIP() {
+		ArrayList<String> arrS2D = new ArrayList<String>();
+
+		String csvFile = "./src/main/resources/IP.csv";
+		BufferedReader br = null;
+		String line = null;
+		String[] csv = null;
+		String SplitBy = "!#";
+
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			while ((line = br.readLine()) != null) {
+				csv = line.split(SplitBy);
+				if (csv.length == 1) {
+					arrS2D.add(csv[0]);
+				} else {
+					for (int i = 0; i < csv.length; i++)
+						arrS2D.add(csv[i]);
+				}
+			} // while ((line = br.readLine()) != null) {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // try {
+
+		// convert
+		String[] IPS2D = new String[arrS2D.size()];
+		arrS2D.toArray(IPS2D);
+		return IPS2D;
+	}
+
+	public static String[][] JSONS2D() {
+		// read IP
+		// final String[] AllIP = { "88.191.179.56", "61.135.248.220" };
+		final String[] AllIP = ReadIP();
+
+		final String[] JSONElementS = { "geoplugin_status",
+				"geoplugin_countryName", "geoplugin_city",
+				"geoplugin_currencyCode", "geoplugin_currencySymbol" };
+		final String[] JSONElementS2 = { "id", "Rate" };
+		String[] urlRate = {
+				"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22",
+				"%22%29&format=json&env=store://datatables.org/alltableswithkeys" };
+
+		String status = null; // 200
+		String countryName = null; // France
+		String city = null; // Paris
+		String currencyCode = null; // EUR
+		String currencySymbol = null; // &#8364; - €
+		String id = null; // USDEUR
+		String rate = null; // 0.7348
+
+		ArrayList<String[]> JSONArList = new ArrayList<String[]>();
+		InputStream is = null;
+		JsonParser parser = null;
+		URL url = null;
+
+		for (int i = 0; i < AllIP.length; i++) {
+			// ############Date###########################################
+			try {
+				url = new URL("http://www.geoplugin.net/json.gp?ip=" + AllIP[i]);
+			} catch (MalformedURLException e1) {
+				// String[][] s = new String[][] { { "1", "CAN'T GET RATE", "" }
+				// };
+				return new String[][] { { "1", "CAN'T GET DATE", "" } }; // s;
+			}
+
+			try {
+				is = url.openStream();
+			} catch (IOException e1) {
+				return new String[][] { { "1", "CAN'T GET DATE", "" } };
+			}
+			parser = Json.createParser(is);
+
+			while (parser.hasNext()) {
+				Event e = parser.next();
+				if (e == Event.KEY_NAME) {
+					if (parser.getString().equals(JSONElementS[0])) {
+						parser.next();
+						status = parser.getString();
+					} else if (parser.getString().equals(JSONElementS[1])) {
+						parser.next();
+						countryName = parser.getString();
+					} else if (parser.getString().equals(JSONElementS[2])) {
+						parser.next();
+						city = parser.getString();
+					} else if (parser.getString().equals(JSONElementS[3])) {
+						parser.next();
+						currencyCode = parser.getString();
+					} else if (parser.getString().equals(JSONElementS[4])) {
+						parser.next();
+						currencySymbol = parser.getString();
+					} // if (parser.getString().equals(JSONElementS[0])) {
+				} // if (e == Event.KEY_NAME) {
+			} // while (parser.hasNext()) {
+				// ############Date###########################################
+
+			// ############Rate###########################################
+			try {
+				url = new URL(urlRate[0] + "USD" + currencyCode + urlRate[1]);
+			} catch (MalformedURLException e1) {
+				// String[][] s = new String[][] { { "1", "CAN'T GET RATE", "" }
+				// };
+				return new String[][] { { "1", "CAN'T GET RATE", "" } }; // s;
+			}
+
+			try {
+				is = url.openStream();
+			} catch (IOException e1) {
+				return new String[][] { { "1", "CAN'T GET RATE", "" } };
+			}
+			parser = Json.createParser(is);
+
+			while (parser.hasNext()) {
+				Event e = parser.next();
+				if (e == Event.KEY_NAME) {
+					if (parser.getString().equals(JSONElementS2[0])) {
+						parser.next();
+						id = parser.getString();
+					} else if (parser.getString().equals(JSONElementS2[1])) {
+						parser.next();
+						rate = parser.getString();
+					} // if (parser.getString().equals(JSONElementS[0])) {
+				} // if (e == Event.KEY_NAME) {
+			} // while (parser.hasNext()) {
+				// ############Rate###########################################
+
+			// запись
+			JSONArList.add(new String[] { AllIP[i], status, countryName, city,
+					currencyCode, currencySymbol, id, rate });
+		} // for(int i = 0; i < AllIP.length; i++)
+
+		// convert
+		String[][] JSONS2D = new String[JSONArList.size()][];
+		JSONArList.toArray(JSONS2D);
+		return JSONS2D;
+	}
+}
+// END
